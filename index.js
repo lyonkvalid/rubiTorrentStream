610ec7a5e63b34d6499fa6362bd162d02d83ece4
lyonkvalid <payouk.mystre@gmail.com>
2022-06-12 02:28:44 +0100

    ...

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import app from "./src/server.js";
 
 // server express app to PORT
-const listener = app.listen(port => {
-    console.log("Running express server at port " + listener.address().port);
+const listener = app.listen(process.env.PORT, port => {
+    console.log("Server running at port " + listener.address().port);
 });
